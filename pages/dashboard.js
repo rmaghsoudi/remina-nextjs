879a35bc66482c6aaca5864208a132f03cbea3de
Romy Maghsoudi <romy.mag9@gmail.com>
2021-03-05 03:12:24 -0500

    basic dashboard setup

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,36 @@
+import styles from '../styles/Home.module.css'
+
 function Dashboard({ user }) {
   return (
-    <div>
-      <h3>{user.username}'s Dashboard</h3>
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          {user.username}'s Dashboard
+        </h1>
+        <p className={styles.description}>
+          A level {user.level} PLACEHOLDER <br />
+          <label htmlFor="xp-bar">XP: </label>
+          <progress id="xp-bar" max={user.xp_to_lvlup} value={user.xp}>  {user.xp}/{user.xp_to_lvlup}</progress>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="/todos" className={styles.card}>
+            <h3>Todos &rarr;</h3>
+            <p>Check out your pending to-dos and create some more!</p>
+          </a>
+
+          <a href="/habits" className={styles.card}>
+            <h3>Habits &rarr;</h3>
+            <p>Check out your habit trends and create new ones!</p>
+          </a>
+
+          <a href="/goals" className={styles.card}>
+            <h3>Goals &rarr;</h3>
+            <p>Check out your goals and create new ones!</p>
+          </a>
+
+        </div>
+      </main>
     </div>
   );
 }
