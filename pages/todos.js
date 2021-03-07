7bb599aca2e7547105948c50a4243b7f7a361613
Romy Maghsoudi <romy.mag9@gmail.com>
2021-03-06 22:39:05 -0500

    adding button to todo page

--- pages/todos.js
+++ pages/todos.js
@@ -6,17 +6,15 @@ function Todos({ todos }) {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Todos Page
-      </h1>
-        <p className={styles.description}>
-          Manage all of your To-dos here.
-        </p>
+        </h1>
+        <button className={styles.card}>Add Todo &#43; </button>
 
         <div className={styles.grid}>
           {todos.map(todo => (
             <div className={styles.card}>
               <h3>{todo.description}</h3>
               <p>XP: {todo.xp}</p> 
-              <input type="checkbox" checked={todo.completed} />
+              <input type="checkbox" defaultChecked={todo.completed} />
             </div>
           ))}
         </div>
