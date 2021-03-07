e875743deac5b9a37837f26452aff982d9b4b2a0
Romy Maghsoudi <romy.mag9@gmail.com>
2021-03-06 23:43:32 -0500

    removing redundant commented code

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -41,19 +41,5 @@ function Dashboard() {
   );
 }
 
-// // This function gets called at build time
-// export async function getStaticProps() {
-//   // Call an external API endpoint to get user
-//   const res = await fetch(`${process.env.API_URL}/users/1`);
-//   const user = await res.json();
-
-//   // By returning { props: { user } }, the Dashboard component
-//   // will receive `user` as a prop at build time
-//   return {
-//     props: {
-//       user,
-//     },
-//   };
-// }
 
 export default Dashboard;
